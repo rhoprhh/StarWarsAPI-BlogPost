74f0aa57874305f6a1810fb658317e608e2a22b5
RHOPRHH <rhoprhh@gmail.com>
2016-06-01 20:53:48 -0400

    finished all basic index and show pages with links

--- part3/swapi/app/controllers/vehicles_controller.rb
+++ part3/swapi/app/controllers/vehicles_controller.rb
@@ -1,2 +1,11 @@
 class VehiclesController < ApplicationController
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
 end
--- part3/swapi/app/models/planet.rb
+++ part3/swapi/app/models/planet.rb
@@ -1,7 +1,7 @@
 class Planet < ActiveRecord::Base
 
   has_many :film_planets
-  has_many :planets, through: :films
+  has_many :films, through: :film_planets
   has_many :characters
   has_many :species, through: :characters
   self.primary_key = 'id'
