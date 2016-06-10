9358f5fe49516de13a28782393b5e43de29b02c0
RHOPRHH <rhoprhh@gmail.com>
2016-06-10 09:12:31 -0400

    finished part 3

--- part3/swapi/app/controllers/characters_controller.rb
+++ part3/swapi/app/controllers/characters_controller.rb
@@ -3,9 +3,7 @@ class CharactersController < ApplicationController
   def index
     @characters = Character.all
   end
-
   def show
     @character = Character.find(params[:id])
   end
-
 end
--- part3/swapi/app/models/starship.rb
+++ part3/swapi/app/models/starship.rb
@@ -1,9 +1,9 @@
 class Starship < ActiveRecord::Base
 
-    has_many :character_starships
-    has_many :characters, through: :character_starships
-    has_many :film_starships
-    has_many :films, through: :film_starships
-    self.primary_key = 'id'
+  has_many :character_starships
+  has_many :characters, through: :character_starships
+  has_many :film_starships
+  has_many :films, through: :film_starships
+  self.primary_key = 'id'
 
 end
